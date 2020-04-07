ac88a1407b4019a5e97540c5492e072650d81366
Nazran Khondokar <nazran@rokomari.com>
2020-04-08 04:38:41 +0600

    dash

--- app/src/main/java/com/bdshikkhok/dashboard/DashboardList.java
+++ app/src/main/java/com/bdshikkhok/dashboard/DashboardList.java
@@ -4,10 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bdshikkhok.R;
@@ -30,11 +41,19 @@ public class DashboardList extends AppCompatActivity {
     TabLayout dashTabLayout;
     @BindView(R.id.fab)
     FloatingActionButton fab;
+    private Context mContext;
+    private Activity mActivity;
+    private PopupWindow mPopupWindow;
+    boolean click = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_list);
         ButterKnife.bind(this);
+
+        mContext = getApplicationContext();
+        mActivity = DashboardList.this;
+
 
         dashBoard();
 
@@ -43,8 +62,52 @@ public class DashboardList extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                //Custom popUp window
+               /* LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+                View customView = inflater.inflate(R.layout.pop_up_window,null);
+                mPopupWindow = new PopupWindow(
+                        customView,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+
+                if(Build.VERSION.SDK_INT>=21){
+                    mPopupWindow.setElevation(5.0f);
+                }
+                ImageButton closeButton = (ImageButton) customView.findViewById(R.id.ib_close);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mPopupWindow.dismiss();
+                    }
+                });
+                mPopupWindow.showAtLocation(fab,Gravity.BOTTOM,100,0);*/
+
+               PopupMenu popupMenu = new PopupMenu(mContext,fab);
+                popupMenu.getMenuInflater().inflate(R.menu.profile_menu,popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch(menuItem.getItemId()){
+                            case R.id.student_updateProfile:
+                                Toast.makeText(mActivity,"okay",Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.signOut:
+                                Toast.makeText(mActivity,"Clicked",Toast.LENGTH_SHORT).show();
+                                return true;
+
+                            default:
+                                return false;
+                        }
+                    }
+                });
+                popupMenu.show();
+
             }
         });
+
+
     }
 
     private void dashBoard() {
@@ -77,6 +140,25 @@ public class DashboardList extends AppCompatActivity {
 
         if(menu_id == R.id.sort){
             Toast.makeText(DashboardList.this,"Sorted",Toast.LENGTH_SHORT).show();
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+            View customView = inflater.inflate(R.layout.pop_up_window,null);
+            mPopupWindow = new PopupWindow(
+                    customView,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            if(Build.VERSION.SDK_INT>=21){
+                mPopupWindow.setElevation(5.0f);
+            }
+            ImageButton closeButton = (ImageButton) customView.findViewById(R.id.ib_close);
+            closeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mPopupWindow.dismiss();
+                }
+            });
+            mPopupWindow.showAtLocation(fab,Gravity.TOP,100,0);
         }
         return super.onOptionsItemSelected(item);
     }
